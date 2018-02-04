db0c453a6bb8067b29d0c42b8d25f724d51fd19c
robert-D-satoshi <sma2ww@gmail.com>
2018-02-04 22:19:19 +0900

    ViewControllerファイル作成

---
+++ mathtraining/mathtraining/UI/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  mathtraining
+//
+//  Created by 山田諭 on 2018/02/04.
+//  Copyright © 2018年 satoshi yamada. All rights reserved.
+//
+
+import Foundation
+
+class SecondeViewController: UIViewController {
+  
+}
